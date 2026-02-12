ce08a8d2a92810dd34010fb3bc0fc5da50cb5cfe
Mahak Saxena <mahaksaxena@gmail.com>
2026-02-12 14:08:14 +0530

    Fix all component imports in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Hero from "../components/Hero";
 import TopOffers from "../components/TopOffers";
 import Testimonials from "../components/Testimonials";
-import AboutUs from "../components/AboutUs";
+import AboutUs from "../componenents/AboutUs"; // typo here
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
 
