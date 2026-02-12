e95529b5816a77dd9728325d416ff57087baf79f
Mahak Saxena <mahaksaxena@gmail.com>
2026-02-12 15:45:34 +0530

    Fix Search.scss import for Netlify build

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import "../styles/Search.scss";
 
 
+
 function SearchResult() {
   const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
