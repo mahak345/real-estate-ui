75a2a4005c30236f74f24a605595d2a2e2d9a96f
Mahak Saxena <mahaksaxena@gmail.com>
2026-02-12 15:09:07 +0530

    Fix component imports and casing in Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
-// Home.jsx
-import Hero from "./Hero";
-import TopOffers from "./TopOffers";
-import Testimonials from "./Testimonials";
-import AboutUs from "./AboutUs";
-import Newsletter from "./Newsletter";
+import Hero from "../components/Hero";
+import TopOffers from "../components/TopOffers";
+import Testimonials from "../components/Testimonials";
+import AboutUs from "./components/AboutUs";
+import Newsletter from "../components/Newsletter";
+
 
 import "./styles/Home.scss";
 import "./styles/Newsletter.scss";
